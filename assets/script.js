221f281fbcafa907aceeb80d358bda5f74adca69
alonzofroman <alonzofroman@gmail.com>
2021-09-30 21:49:57 -0700

    Created ajax functions to gather variables and plug them into another ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,40 @@
-//Call the API to gather all weather data
-// fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={f4fa96020f2282301cd8312fc675da98}'{
+//Call API for geocode
+// http://api.openweathermap.org/geo/1.0/direct?q= {city name},{state code},{country code} &limit={limit} &appid=f4fa96020f2282301cd8312fc675da98
+// https://api.openweathermap.org/data/2.5/onecall?lat= {lat} &lon= {lon}&exclude={part} &appid={API key}
 
-// })
+//Variables to construct API requests
+var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=';
+var lonPar = '&lon=';
+var searchLimit = '&limit=1';
+var inFar ='&units=imperial'
+var apiKey = '&appid=f4fa96020f2282301cd8312fc675da98';
+var geoLat = [];
+var geoLon = []; 
+
+//When search button is clicked, search for that city
+$("#search-button").click(function(){
+    var cityName = $("#search-bar").val().trim();
+
+    $.ajax({
+        url: geoUrl + cityName + searchLimit + apiKey,
+        method: 'GET',
+        datatype: 'JSON',
+    }).then(function(geoData){
+        geoLat.length = 0;
+        geoLon.length = 0;
+    
+        geoLat.push(geoData[0].lat);
+
+        geoLon.push(geoData[0].lon);
+    }).then(function(){
+        
+        $.ajax({
+            url: oneCallUrl + geoLat + lonPar + geoLon + inFar + apiKey,
+            method: 'GET',
+        }).then(function(oneData){
+            console.log(oneData.current.temp);
+        })
+    })
+
+})
