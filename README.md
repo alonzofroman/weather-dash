cf30a0fa0993f13fd24a5e6381c5c43f077b6ed5
alonzofroman <alonzofroman@gmail.com>
2021-09-30 17:19:35 -0700

    Created template for weather inputs in html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # weather-dash
+
+
+## Resources/Acknowledgements
+Font Awesome (https://fontawesome.com/v4.7/)
---
+++ assets/script.js
@@ -0,0 +1,4 @@
+//Call the API to gather all weather data
+// fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={f4fa96020f2282301cd8312fc675da98}'{
+
+// })
