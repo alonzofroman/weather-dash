0f518b5a2d9bf4df6e706876eed1608e51a8a577
alonzofroman <alonzofroman@gmail.com>
2021-10-01 17:55:15 -0700

    Finished function to display weather of next five days

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,7 @@ $("#search-button").click(function(){
     cityName.push($("#search-bar").val().trim());
     console.log(cityName);
 
+    //After city name is entered, find lat and lon
     $.ajax({
         url: geoUrl + cityName + searchLimit + apiKey,
         method: 'GET',
@@ -33,6 +34,7 @@ $("#search-button").click(function(){
         geoLon.push(geoData[0].lon);
     }).then(function(){
         
+        //With lat and lon variables, find the city from the city name search in One Call API
         $.ajax({
             url: oneCallUrl + geoLat + lonPar + geoLon + inFar + apiKey,
             method: 'GET',
@@ -43,25 +45,47 @@ $("#search-button").click(function(){
             var currentUvi = oneData.current.uvi;
             console.log(oneData);
 
+            //Add current weather data from API call to the weather dashboard
             $("#city-today").text(cityName);
             $("#temp-today").text('Temperature: ' + currentTemp);
             $("#humid-today").text('Humidity: ' + currentHumid);
             $("#wind-today").text('Wind Speed: ' + currentWind);
             $("#uvi-today").text('UV Index: ' + currentUvi);
             
+
+            //Display temp and humidity for next day
             var day1Temp = oneData.daily[1].temp.day;
             var day1Humid = oneData.daily[1].humidity;
-
-
-            $("#day1temp").text('Temperature: ' + day1Temp);
+            $("#day1temp").text('Temp: ' + day1Temp);
             $("#day1humid").text('Humidity: ' + day1Humid);
+
+            //Two days ahead
+            var day2Temp = oneData.daily[2].temp.day;
+            var day2Humid = oneData.daily[2].humidity;
+            $("#day2temp").text('Temp: ' + day2Temp);
+            $("#day2humid").text('Humidity: ' + day2Humid);
+
+            //Three days ahead
+            var day3Temp = oneData.daily[3].temp.day;
+            var day3Humid = oneData.daily[3].humidity;
+            $("#day3temp").text('Temp: ' + day3Temp);
+            $("#day3humid").text('Humidity: ' + day3Humid);
+
+            //Four days ahead
+            var day4Temp = oneData.daily[4].temp.day;
+            var day4Humid = oneData.daily[4].humidity;
+            $("#day4temp").text('Temp: ' + day4Temp);
+            $("#day4humid").text('Humidity: ' + day4Humid);
+
+            //Five days ahead
+            var day5Temp = oneData.daily[5].temp.day;
+            var day5Humid = oneData.daily[5].humidity;
+            $("#day5temp").text('Temp: ' + day5Temp);
+            $("#day5humid").text('Humidity: ' + day5Humid);
+
 
         })
     })
 
 })
 
-
-function displayCurrent() {
-
-}
